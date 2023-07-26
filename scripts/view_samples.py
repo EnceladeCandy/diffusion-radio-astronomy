8c89a8e2f929d960a2864566c635c94a6c6def86
EnceladeCandy <noe.dia@umontreal.ca>
2023-07-26 18:14:57 -0400

    Changes match now the posterior_probes file, changed bad names for some args

--- scripts/view_samples.py
+++ scripts/view_samples.py
@@ -7,16 +7,38 @@ import h5py
 plt.style.use("dark_background")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+"""
+This script creates plots to view the posterior samples in the samples_probes folder or the samples folder.
+
+Args:
+sigma_likelihood = sqrt(variance) of the gaussian noise added in the forward model
+idx = indice of the image in the probes dataset
+num_samples = number of posterior samples to create for the second plot
+samples_folder = samples_probes or samples (samples_probes = posterior samples using the real prior, samples = posterior samples using a sample from the learnt prior)
+
+"""
 
 def main(args):
-    # Recovering the image from the dataset: 
+    def preprocess_probes_g_channel(img, inv_link = False):  # channel 0
+        img = torch.clamp(img, 0, 1.48)
+        
+        if inv_link:
+            img = 2 * img / 1.48 - 1.
+        return img
+    
+    # Image from the probes dataset: 
     dir_galaxies = "/home/noedia/projects/rrg-lplevass/data/probes.h5"
     hdf = h5py.File(dir_galaxies, "r")
     hdf.keys()
     dataset = hdf['galaxies']
 
+    # Idx of the image in the dataset and 
+    idx = args.idx
+    sigma_likelihood = args.sigma_likelihood
+
     # Importing the posterior samples
-    samples, sigma_likelihood = torch.load("../../samples/"+ args.samples_dir + ".pt")
+    path = f"../../"+ args.samples_folder + f"/sigma_{sigma_likelihood:.1g}/"
+    samples = torch.load(path + f"idx_{idx}.pt", map_location=device)
     num_samples = samples.shape[0]
     img_size = int((samples.shape[1])**0.5) # Assuming images with height = width
     samples = samples.reshape(-1, img_size, img_size)
@@ -31,25 +53,31 @@ def main(args):
         return torch.fft.ifft2(x, norm = "ortho")
 
 
-    psf = torch.load("../psf64.pt")
-    img = dataset[101,...,0]
-    img = F.avg_pool2d(torch.tensor(img[None, None, ...]), (4, 4))[0, 0].to(device)
-    img = torch.load("../ground_truth.pt")
+    psf = torch.load("../psf64.pt", map_location = device)
+    img = torch.tensor(dataset[0, ..., 1]) # green channel
+    img = preprocess_probes_g_channel(img) # probes (N, 256, 256, 3)
+    img = F.avg_pool2d(img[None, None, ...], (4, 4))[0, 0].to(device)
+    #img = torch.load("../ground_truth.pt")
+    #img = torch.load("../../prior.pt", map_location = device)[idx]
     img_size = img.shape[-1]
 
+    # Calculating the sampled visibilities: V_s = FT(psf) * FT(img)
     vis_full = ft(img).flatten()
     sampling_function= ft(torch.fft.ifftshift(psf)).flatten()
     vis_sampled = sampling_function * vis_full
 
     vis_sampled = vis_sampled.flatten()
-    vis_sampled = torch.cat([vis_sampled.real, vis_sampled.imag])
+    vis_sampled = torch.cat([vis_sampled.real, vis_sampled.imag]) # trik for torch
 
     y_dim = len(vis_sampled)  
     dist_likelihood = MultivariateNormal(loc = torch.zeros(y_dim).to(device), covariance_matrix=sigma_likelihood **2 * torch.eye(y_dim).to(device))
     eta = dist_likelihood.sample([])
-
+    
+    # Creaing an observation
     y = vis_sampled + eta 
-    fig, axs = plt.subplots(1, 2, figsize = (10, 4))
+
+    print("Plotting naive reconstruction...\n")
+    fig, axs = plt.subplots(1, 2, figsize = (8, 4))
     dirty_image_noise = ift((y[:img_size**2] + 1j * y[img_size**2:]).reshape(img_size, img_size)).real
 
     for i in range(len(axs)): 
@@ -60,19 +88,19 @@ def main(args):
     axs[1].imshow(dirty_image_noise.cpu(), cmap = "hot")
     axs[1].set_title("Dirty image with noise")
     plt.subplots_adjust(wspace = 0.1)
-    plt.savefig(f"../../images/naive_rec_{sigma_likelihood:.1g}.jpeg", bbox_inches = "tight", pad_inches = 0.1)
+    fig.suptitle(r"$\sigma_{lh}$ = " + f"{sigma_likelihood:.1g}", y = 0.1)
+    plt.savefig(f"../../images/naive_rec/{sigma_likelihood:.1g}_{idx}.jpeg", bbox_inches = "tight", pad_inches = 0.1)
 
 
-
+    print("Plotting posterior samples...\n")
     ############ SECOND PLOT ################### 
     # Creating the plot
-    grid_size = int((num_samples)**0.5) # Square grid, same number of rows and cols:
-    print(grid_size)
+    grid_size = int((args.num_samples) ** 0.5)
     if grid_size==1:
         fig = plt.figure(figsize= (8,8), dpi = 150)
         plt.imshow(samples[0].cpu(), cmap = "hot")
         plt.axis("off")
-        plt.savefig(f"../../images/posterior_{sigma_likelihood:.1g}.jpg", bbox_inches = "tight", pad_inches = 0.1)
+        plt.savefig(f"../../images/samples/posterior_{sigma_likelihood:.1g}_{idx}.jpg", bbox_inches = "tight", pad_inches = 0.1)
 
     else:
         fig, axs = plt.subplots(grid_size, grid_size, figsize = (10, 10), dpi = 150)
@@ -83,14 +111,17 @@ def main(args):
                 axs[i, j].imshow(samples[k].cpu(), cmap = "hot")
                 axs[i, j].axis("off")
                 k += 1
-        plt.title(r"$\sigma_{lh}$ = " + f"{sigma_likelihood:.1g}")
+        fig.suptitle(r"$\sigma_{lh}$ = " + f"{sigma_likelihood:.1g}", y = 0.1)
         plt.subplots_adjust(wspace = 0.1, hspace = 0.1)
-        plt.savefig(f"../../images/posterior_{sigma_likelihood:.1g}.jpg", bbox_inches = "tight", pad_inches = 0.1)
+        plt.savefig(f"../../images/samples/posterior_{sigma_likelihood:.1g}_{idx}.jpg", bbox_inches = "tight", pad_inches = 0.1)
 
 if __name__ == "__main__": 
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("--samples_dir",        required = True,   default = "",                 help = "Directory to save the samples (must end with /)")
+    parser.add_argument("--sigma_likelihood",        required = True,    type = float)
+    parser.add_argument("--idx",                required = True,    type = int)
+    parser.add_argument("--num_samples",    required = True,    type=int, help = "Number of samples to view in the plot (ideally a perfect square)")
+    parser.add_argument("--samples_folder",        required = False,    default ="samples_probes")
     args = parser.parse_args()
     main(args) 
 
